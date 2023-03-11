5df95242800b4e32f3e8dcecff70a15d37f9f97c
Amier Putra Azhar <86624398+amierputra@users.noreply.github.com>
2023-03-11 14:41:02 +0800

    redesign guest/auth views

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -16,10 +16,21 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-start mt-4">
             <x-primary-button>
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
     </form>
+    <div class="mt-6">
+        <a href="/">
+            <button class="border-t-2 border-black pt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
+
+                <span class="inline-block ml-1 text-sm">Back to {{ env('APP_DOMAIN') }}</span>
+            </button>
+        </a>
+    </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,7 +39,7 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
@@ -49,4 +49,15 @@
             </x-primary-button>
         </div>
     </form>
+    <div class="mt-10">
+        <a href="/" class="hover:text-blue-700">
+            <button class="border-t-2 border-black pt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
+
+                <span class="inline-block ml-1 text-sm">Back to {{ env('APP_DOMAIN') }}</span>
+            </button>
+        </a>
+    </div>
 </x-guest-layout>
